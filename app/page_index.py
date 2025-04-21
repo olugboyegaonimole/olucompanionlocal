70c5e5deea23c7ea34ae24ac70776e5f4ee910f4
olugboyega onimole <olugboyegaonimole@gmail.com>
2025-04-21 09:51:23 +0100

    updates

---
+++ app/page_index.py
@@ -0,0 +1,53 @@
+# page_index.py
+
+page_index = [
+    {"name": "Synonyms", "url": "/synonyms", "content": "similar words, equivalent terms, alternative words"},
+    {"name": "Concise Expressions", "url": "/concise_expressions", "content": "shorter versions, brevity, clear phrases"},
+    {"name": "Word Substitutions", "url": "/word_substitutions", "content": "replace words, switch terms, better word choices"},
+    {"name": "Simplifications", "url": "/simplifications", "content": "easy words, plain language, simpler wording"},
+    {"name": "One-Word Summaries", "url": "/one_word_summaries", "content": "summarize in one word, condense phrases"},
+
+    {"name": "Metaphors", "url": "/metaphors", "content": "figurative language, indirect comparisons, literary devices"},
+    {"name": "Similes", "url": "/similes", "content": "like or as, comparisons, poetic expressions"},
+    {"name": "Idioms", "url": "/idioms", "content": "common sayings, expressions with hidden meanings"},
+    {"name": "Personifications", "url": "/personifications", "content": "give human traits to objects, poetic description"},
+    {"name": "Hyperboles", "url": "/hyperboles", "content": "exaggeration, dramatic expression, not literal"},
+
+    {"name": "Wisdom Proverbs", "url": "/wisdom_proverbs", "content": "wise sayings, ancient advice, traditional knowledge"},
+    {"name": "Cultural Proverbs", "url": "/cultural_proverbs", "content": "sayings from around the world, traditions, folk wisdom"},
+    {"name": "Motivational Proverbs", "url": "/motivational_proverbs", "content": "uplifting sayings, encouragement, positive quotes"},
+    {"name": "Humorous Proverbs", "url": "/humorous_proverbs", "content": "funny sayings, witty proverbs, light-hearted advice"},
+    {"name": "Timeless Sayings", "url": "/timeless_sayings", "content": "classic proverbs, passed through generations"},
+
+    {"name": "Inspirational Quotes", "url": "/inspirational_quotes", "content": "encouraging sayings, uplift, motivate"},
+    {"name": "Love & Friendship Quotes", "url": "/love_friendship_quotes", "content": "affectionate words, friendship sayings, romantic quotes"},
+    {"name": "Success & Ambition Quotes", "url": "/success_ambition_quotes", "content": "career, goals, achievements, drive"},
+    {"name": "Birthday & Celebration Quotes", "url": "/birthday_celebration_quotes", "content": "party sayings, celebration lines, birthday wishes"},
+    {"name": "Quotes on Life Lessons", "url": "/life_lessons_quotes", "content": "wisdom, reflection, growth, advice"},
+
+    {"name": "Animal Similes", "url": "/animal_similes", "content": "as sly as a fox, like a lion, animal-based comparisons"},
+    {"name": "Nature-Inspired Similes", "url": "/nature_inspired_similes", "content": "as bright as the sun, earthy similes"},
+    {"name": "Emotional Similes", "url": "/emotional_similes", "content": "as happy as a clam, feeling-based comparisons"},
+    {"name": "Funny Similes", "url": "/funny_similes", "content": "humorous comparisons, silly similes, like a chicken on a trampoline"},
+    {"name": "Poetic Comparisons", "url": "/poetic_comparisons", "content": "beautiful imagery, poetic similes, elegant comparisons"},
+
+    {"name": "Internet & Texting Abbreviations", "url": "/internet_texting_abbreviations", "content": "LOL, BRB, texting short forms, online lingo"},
+    {"name": "Scientific & Medical Prefixes", "url": "/scientific_medical_prefixes", "content": "bio-, hydro-, anatomy terms, science roots"},
+    {"name": "Business & Corporate Acronyms", "url": "/business_corporate_acronyms", "content": "CEO, ROI, corporate language"},
+    {"name": "Educational & Academic Shortforms", "url": "/educational_academic_shortforms", "content": "PhD, GPA, school-related terms"},
+    {"name": "Government & Legal Terms", "url": "/government_legal_terms", "content": "FBI, legal acronyms, political terms"},
+
+    {"name": "World Landmarks", "url": "/world_landmarks", "content": "Eiffel Tower, pyramids, famous monuments"},
+    {"name": "Unique Natural Wonders", "url": "/unique_natural_wonders", "content": "Grand Canyon, Northern Lights, nature's beauty"},
+    {"name": "Extreme Weather Facts", "url": "/extreme_weather_facts", "content": "storms, coldest, hottest, weather trivia"},
+    {"name": "Capital Cities & Countries", "url": "/capital_cities_countries", "content": "world capitals, countries, maps"},
+    {"name": "Fun Geography Trivia", "url": "/fun_geography_trivia", "content": "interesting geography facts, weird places"},
+
+    {"name": "Grammar Essentials", "url": "/grammar_essentials", "content": "parts of speech, punctuation, grammar rules"},
+    {"name": "Literary Devices", "url": "/literary_devices", "content": "metaphor, simile, alliteration, foreshadowing"},
+    {"name": "Common Writing Mistakes", "url": "/common_writing_mistakes", "content": "typos, grammar issues, wrong word use"},
+    {"name": "Exam Tips & Tricks", "url": "/exam_tips_tricks", "content": "study advice, memory tricks, revision hacks"},
+    {"name": "Reading Comprehension Strategies", "url": "/reading_comprehension_strategies", "content": "skimming, scanning, answering questions"},
+
+    {"name": "Major Historical Events", "url": "/world_history_events", "content": "columbus, napoleon, ww2, civil war, historical facts"}
+]
